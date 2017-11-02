633dc92fe0b2253e2c5dd4984a4d57a7dfec9986
Unknown <laurancy.dorian@gmail.com>
2017-11-02 16:15:49 +0100

    Attributs CaseObjectif
    Constructeur de base + getter and setter

--- CaseObjectif.java
+++ CaseObjectif.java
@@ -1,5 +1,19 @@
 package ricochetRobot;
 
 public class CaseObjectif extends Case {
+	private int robotRequis;
+
+	public CaseObjectif() {
+		super();
+		robotRequis = (int) (Math.random() * 4); // Attribut aléatoirement un des 4 Robots
+	}
+
+	public int getRobotRequis() {
+		return robotRequis;
+	}
+
+	public void setRobotRequis(int robotRequis) {
+		this.robotRequis = robotRequis;
+	}
 
 }
