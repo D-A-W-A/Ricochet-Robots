ce969d62e9b208ffe37bd321375e7b6f1319d7fe
Unknown <laurancy.dorian@gmail.com>
2017-11-02 16:01:26 +0100

    Ajout des Attributs de Case

--- Case.java
+++ Case.java
@@ -1,5 +1,13 @@
 package ricochetRobot;
 
 public class Case {
+	private static int nbCases = 0;
+	private int idCase;
+	private Case[] caseVoisines = new Case[4]; // Taille min : 1 et max : 4
+
+	public Case() {
+		nbCases++;
+		this.idCase = nbCases;
+	}
 
 }
