794d820fb0ddf969524f1ccb8f32dd83d55be713
Dodo <laurancy.dorian@gmail.com>
2017-11-03 20:40:19 +0100

    Ajout de commentaires dans Case
    Et ajout de l'attribut Occupe

--- Case.java
+++ Case.java
@@ -1,9 +1,20 @@
 package ricochetRobot;
 
+/**
+ * Une Case simple du Plateau<br>
+ * Elle possède une id unique, un attribut occupe et un tableau de 4 Case next<br><br>
+ * Stratégie : 
+ * C'est une structure réccursive sous forme de graphe orienté. Chaque Case possède 4 Cases next qui ne sont pas null.
+ * Si une Case possède au moins une caseNext = Null, alors cette case est une Case vide.<br>
+ * Une case vide n'est pas atteignable par un Robot : On peut donc considérer une Case vide comme un Mur
+ * @author Dorian
+ *
+ */
 public class Case {
 	private static int nbCases = 0;
 	private int idCase;
-	private Case[] caseVoisines = new Case[4]; // Taille min : 1 et max : 4
+	private Case[] caseNext = new Case[4];
+	private boolean occupe;
 
 	public Case() {
 		nbCases++;
