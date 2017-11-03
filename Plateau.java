ab1fb4735965e87adfd1f89093e2f363d0b533a2
Dodo <laurancy.dorian@gmail.com>
2017-11-03 21:07:47 +0100

    Commentaires

--- Plateau.java
+++ Plateau.java
@@ -1,11 +1,19 @@
 package ricochetRobot;
 
 /**
- * Plateau du jeu, composé de Cases et de Robots
- * <br><br>
- * Stratégie : La génération du Plateau crée des cases et attribue à chaque case les case next. Il doit également créer au moins une case de type CaseObjectif.
- * <br>Les murs sont représentés par des Cases vides mais ces cases vides n'apparaissent pas dans tabCases. 
- * <br>Graphiquement : Les murs seront retrouvés en faisant un test à chaque case si ses next sont vides, si oui, un mur apparaitra à coté de la case.
+ * Plateau du jeu, composé de Cases et de Robots <br>
+ * <br>
+ * Stratégie : La génération du Plateau crée des cases et attribue à chaque case
+ * les case next. Il doit également créer au moins une case de type
+ * CaseObjectif. <br>
+ * <br>
+ * S'il y a plusieurs Robots : un Robot placé sur une Case du plateau redéfinira
+ * les CaseNext de toutes les cases horizontales et verticales.<br><br>
+ * Les murs sont représentés par des Cases vides mais ces cases vides
+ * n'apparaissent pas dans tabCases. <br>
+ * Graphiquement : Les murs seront retrouvés en faisant un test à chaque case si
+ * ses next sont vides, si oui, un mur apparaitra à coté de la case.
+ * 
  * @author Dorian
  *
  */
@@ -19,4 +27,3 @@ public class Plateau {
 	}
 
 }
-
