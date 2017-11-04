29c364e28dd125b65a97a0d709569290fcc6bec9
Dodo <laurancy.dorian@gmail.com>
2017-11-04 15:45:11 +0100

    Suppression des accents Robot (bug)

--- Robot.java
+++ Robot.java
@@ -1,17 +1,17 @@
 package ricochetRobot;
 
 /**
- * Un pion déplaçable par le joueur. Un robot ne peut se déplacer que tout droit
- * jusqu’à atteindre un obstacle (un bord du plateau, un mur ou un autre Robot).
+ * Un pion deplacable par le joueur. Un robot ne peut se deplacer que tout droit
+ * jusqu'a atteindre un obstacle (un bord du plateau, un mur ou un autre Robot).
  * <br>
  * <br>
- * Stratégie : Le Robot se trouve sur une Case Aléatoire du plateau, et peut se
- * déplacer sur l'une des CaseNext de cette case, à condition qu'elle ne soit
+ * Strategie : Le Robot se trouve sur une Case Aleatoire du plateau, et peut se
+ * deplacer sur l'une des CaseNext de cette case, a condition qu'elle ne soit
  * pas vide (mur) Le but du Robot 1 est d'atteindre la CaseObjectif en un
  * minimum de coup. <br>
  * <br>
- * Note : La première version du programme se fait avec 1 unique Robot, nous
- * n'avons pas besoin gérer les colisions entre Robots ou les modifications des
+ * Note : La premiere version du programme se fait avec 1 unique Robot, nous
+ * n'avons pas besoin gerer les colisions entre Robots ou les modifications des
  * CaseNext
  * 
  * @author Dorian
