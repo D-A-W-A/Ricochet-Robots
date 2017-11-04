6ac194a98ceb34ed355b16bf03b88030d17fc538
Alcynos <33374011+Alcynos@users.noreply.github.com>
2017-11-04 15:42:43 +0100

    Constructeur vide robot

--- Robot.java
+++ Robot.java
@@ -1,17 +1,17 @@
 package ricochetRobot;
 
 /**
- * Un pion déplaçable par le joueur. Un robot ne peut se déplacer que tout droit
- * jusqu’à atteindre un obstacle (un bord du plateau, un mur ou un autre Robot).
+ * Un pion dÃ©plaÃ§able par le joueur. Un robot ne peut se dÃ©placer que tout droit
+ * jusquâ€™Ã  atteindre un obstacle (un bord du plateau, un mur ou un autre Robot).
  * <br>
  * <br>
- * Stratégie : Le Robot se trouve sur une Case Aléatoire du plateau, et peut se
- * déplacer sur l'une des CaseNext de cette case, à condition qu'elle ne soit
+ * StratÃ©gie : Le Robot se trouve sur une Case AlÃ©atoire du plateau, et peut se
+ * dÃ©placer sur l'une des CaseNext de cette case, Ã  condition qu'elle ne soit
  * pas vide (mur) Le but du Robot 1 est d'atteindre la CaseObjectif en un
  * minimum de coup. <br>
  * <br>
- * Note : La première version du programme se fait avec 1 unique Robot, nous
- * n'avons pas besoin gérer les colisions entre Robots ou les modifications des
+ * Note : La premiÃ¨re version du programme se fait avec 1 unique Robot, nous
+ * n'avons pas besoin gÃ©rer les colisions entre Robots ou les modifications des
  * CaseNext
  * 
  * @author Dorian
@@ -21,8 +21,11 @@ public class Robot {
 	private int idRobot;
 	private boolean selectione;
 	private int[] position = new int[2];
+	private static int nbRobot = 0;
 
 	public Robot() {
+		nbRobot++;
+		this.idRobot = nbRobot;
 		// TODO
 	}
 
