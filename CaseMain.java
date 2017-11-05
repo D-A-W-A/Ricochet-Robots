fadbe3c34fcff700c25e30d14ef3995c608a1759
Dodo <laurancy.dorian@gmail.com>
2017-11-05 17:50:21 +0100

    Case : Test de estVide()

--- CaseMain.java
+++ CaseMain.java
@@ -3,7 +3,21 @@ package ricochetRobot;
 public class CaseMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		// Test de estVide()
+		Case c = new Case();
+		System.out.println("c est vide ? " + c.estVide());
+		
+		Case c1 = new Case();
+		c.setCaseNextGauche(c1);
+		System.out.println("Ajout d'un CaseNextGauche, c est vide ? " + c.estVide());
+
+		Case[] tabCase = new Case[4];
+		for (int i = 0; i < 4; i++) {
+			tabCase[i] = new Case();
+		}
+		c.setCaseNext(tabCase);
+		System.out.println("Ajout d'un tableau de CaseNext, c est vide ? " + c.estVide());
 
 	}
 
