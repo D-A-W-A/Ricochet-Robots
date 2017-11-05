9fad08afc0ef1d9236b1d0e7582ae66c2d856259
Dodo <laurancy.dorian@gmail.com>
2017-11-05 20:06:04 +0100

    Plateau :  determinerMurRandom(int n)

--- Plateau.java
+++ Plateau.java
@@ -1,5 +1,7 @@
 package ricochetRobot;
 
+import java.util.Random;
+
 /**
  * Plateau du jeu, composé de Cases et de Robots <br>
  * <br>
@@ -19,27 +21,34 @@ package ricochetRobot;
  *
  */
 public class Plateau {
-	
+
 	//////// ATTRIBUTS ///////////
-	
-	
+
 	private int taille;
 	private Case[][] tabCases;
 	private Robot[] tabRobots;
 
-	
 	/////// CONSTRUCTEURS /////////
-	
+
 	/**
-	 * Constructeur Vide : Cree un plateau de taille 16 et alloue la mémoire pour tabCases et tabRobots
+	 * Constructeur Vide : Cree un plateau de taille 16 et alloue la mémoire pour
+	 * tabCases et tabRobots
 	 */
 	public Plateau() {
 		taille = 16;
 		tabCases = new Case[16][16];
 		tabRobots = new Robot[1];
 	}
-		
-	
+
+	/**
+	 * Cree un plateau de taille n et alloue la mémoire pour tabCases et tabRobots
+	 */
+	public Plateau(int n) {
+		taille = n;
+		tabCases = new Case[n][n];
+		tabRobots = new Robot[1];
+	}
+
 	/////// GETTERS AND SETTERS ////////
 
 	public int getTaille() {
@@ -57,8 +66,8 @@ public class Plateau {
 	public void setTabCases(Case[][] tabCases) {
 		this.tabCases = tabCases;
 	}
-	
-	public void setLigne (int i, Case[] t) {
+
+	public void setLigne(int i, Case[] t) {
 		tabCases[i] = t;
 	}
 
@@ -69,39 +78,66 @@ public class Plateau {
 	public void setTabRobots(Robot[] tabRobots) {
 		this.tabRobots = tabRobots;
 	}
-	
-	
+
 	/////// METHODES /////////
-	
-	
+
 	/**
 	 * Initialise la ligne i du plateau avec des cases et des murs de tous les cotés
 	 */
 	public void initLigne(int i) {
 		Case[] tCases = new Case[this.getTaille()];
-		for (int j = 0; i<this.getTaille(); i++) {
+		for (int j = 0; i < this.getTaille(); i++) {
 			tCases[j] = Case.creerCase();
 		}
 		this.setLigne(i, tCases);
-		
+
 	}
+
 	/**
 	 * Genere un plateau classique : 16*16 cases, murs prédéfinis
+	 * 
 	 * @return le plateau genere
 	 */
 	public static Plateau genererPlateauClassique() {
 		return null;
 	}
-	
+
 	/**
-	 * Genere un plateau de taille n de manière Aléatoire : 12% de chances qu'une case ait au moins un mur
+	 * Genere un plateau de taille n de manière Aleatoire : 12% de chances qu'une
+	 * case ait au moins un mur (donc 3 % par coté)<br>
+	 * Utilise :<br> 
+	 * @see initLigne(int i)<br>
+	 * @see determinerMurRandom(int n)
 	 * 
 	 * @param n
 	 * @return
 	 */
 	public static Plateau genererPlateauRandom(int n) {
-		Plateau p = new Plateau();
-		return null;
+		Plateau p = new Plateau(n);
+		for (int i = 0; i < n; i++) {
+			p.initLigne(i);
+		}
+		
+	}
+	
+	/**
+	 * Determine de façon aleatoire quelle case aura un mur. Une case a 3 % de chance d'avoir un mur
+	 * @param n la taille du plateau
+	 */
+	private static boolean[][] determinerMurRandom(int n) {
+		Random r = new Random();
+		int tirage;
+		boolean[][] t = new boolean [n][n];
+		for (int i=0; i<n; i++) {
+			for (int j=0; j<n; j++) {
+				tirage = r.nextInt(100);
+				if (tirage <3)
+					t[i][j] = true;
+				else 
+					t[i][j] = false;
+			}
+		}
+		return t;
 	}
 
 }
